9011f5ac6118234dd4ca43b2359045e4d6ee6eb8
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-22 11:42:15 +0100

    ajoute de methode illustrant les life cycle de l'activity. ajout de composant pour jouer.

--- app/src/main/java/education/cccp/lifecycles/MainActivity.kt
+++ app/src/main/java/education/cccp/lifecycles/MainActivity.kt
@@ -1,11 +1,104 @@
 package education.cccp.lifecycles
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var amountTextView: TextView
+
+    //    La méthode onCreate est une méthode du cycle de vie d'android
+    //    Elle se déclenche le moment ou l'activité est lancée
+    //    Les initialisations s'effectuent dans cette méthode
+    //    l'interface d'activité(layout) est gonflée ("inflated")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        amountTextView = findViewById(R.id.textView2)
+        if (savedInstanceState != null && !savedInstanceState.isEmpty) {
+            amountTextView.text = savedInstanceState
+                .getString("montant", "")
+        }
+        Log.d(
+            MainActivity::class.java.name,
+            "onCreate()"
+        )
+    }
+
+    // La methode est appelé lorsque l'activité devient visible.
+    override fun onStart() {
+        super.onStart()
+        Log.d(
+            MainActivity::class.java.name,
+            "onStart()"
+        )
+    }
+
+    // La méthode obtient le focus(on peut agir avec l'activité)
+    override fun onResume() {
+        super.onResume()
+        Log.d(
+            MainActivity::class.java.name,
+            "onResume()"
+        )
+    }
+
+    // L'activité a perdu le focus(elle n'est pas/plus au premier plan)
+    override fun onPause() {
+        super.onPause()
+        Log.d(
+            MainActivity::class.java.name,
+            "onPause()"
+        )
+    }
+
+    // L'activité n'est plus visible pour l'utilisateur,
+    // c'est dans cette méthode qu'on libere les resources
+    // qui ne sont plus nécessaires.
+    // c'est le moment ou enregistrer tous les etats a persister
+    // au de la du cycle de vie de l'activité.
+    override fun onStop() {
+        super.onStop()
+        Log.d(
+            MainActivity::class.java.name,
+            "onStop()"
+        )
+    }
+
+    // L'activité est sur le point d'etre detruite(parce que terminée),
+    // On procede a un nettoyage des resources
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(
+            MainActivity::class.java.name,
+            "onDestroy()"
+        )
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(
+            MainActivity::class.java.name,
+            "onRestart()"
+        )
+    }
+
+    // permet de sauvegarder des données utilisateur
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(
+            MainActivity::class.java.name,
+            "onSaveInstanceState()"
+        )
+        outState.run {
+            putString("montant", amountTextView.text.toString())
+        }
+    }
+
+    fun calculate(view: View) {
+        amountTextView.text = "le montant"
     }
 }
